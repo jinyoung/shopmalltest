35fd6eb8679677394ef68a711814fd6d1a1adaa0
Jinyoung Jang <jyjang@uengine.org>
2022-08-19 02:32:58 +0000

    c

--- delivery/src/main/java/shoppingmall/domain/Delivery.java
+++ delivery/src/main/java/shoppingmall/domain/Delivery.java
@@ -25,11 +25,12 @@ public class Delivery {
     }
 
     public static void startDelivery(OrderPlaced orderPlaced) {
-        /** Example 1:  new item 
+        /** Example 1:  new item*/ 
         Delivery delivery = new Delivery();
+        
         repository().save(delivery);
 
-        */
+        
 
         /** Example 2:  finding and process
         
